39999dc56f8ca8245bd56f784d00632502bc52a8
Berkayszk <testmail>
2023-12-31 17:08:34 +0300

    Tittles

--- Fi Wo/Controllers/HomeViewController.swift
+++ Fi Wo/Controllers/HomeViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    let sectionTitles : [String] = ["Trending Movies","Popular","Trending Tv","Upcoming Movies","Top Rated"]
+    
     private let homeFeedTables : UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
         table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
@@ -18,12 +20,24 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(homeFeedTables)
-        
+        configureNavBar()
         homeFeedTables.dataSource = self
         homeFeedTables.delegate = self
         
-        homeFeedTables.tableHeaderView = UIView(frame: CGRect(x:0, y: 0, width: view.bounds.width,height: 450))
+        let headerview = HeroHeaderUIView(frame: CGRect(x:0, y:0, width: view.bounds.width, height: 450))
+        homeFeedTables.tableHeaderView = headerview
 
+    }
+    private func configureNavBar() {
+        var image = UIImage(named: "fiwoLogo")
+        image = image?.withRenderingMode(.alwaysOriginal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+        
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+        navigationController?.navigationBar.tintColor = .white
     }
     
     override func viewDidLayoutSubviews() {
@@ -31,16 +45,30 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         homeFeedTables.frame = view.bounds
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
             return UITableViewCell()
         }
         return cell
                 
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitles.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else {return}
+        header.textLabel?.font = .systemFont(ofSize: 16, weight: .semibold)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.textColor = .white
+        
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -52,8 +80,14 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return 40
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 20
+
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defoultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defoultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
     }
+     
 
 }
