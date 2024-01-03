26ef7df258bea05767f7e141de16753a381aa47a
Berkayszk <testmail>
2024-01-03 23:31:14 +0300

    .

---
+++ Fi Wo/Manager/APICaller.swift
@@ -0,0 +1,20 @@
+//
+//  APICaller.swift
+//  Fi Wo
+//
+//  Created by Berkay Sazak on 3.01.2024.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "87be3a25620bfec5b2c45e1fd566b362"
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion : @escaping (String) -> Void) {
+        
+    }
+}
