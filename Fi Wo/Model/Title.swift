472e2ad09c738e1323f5e2f723b84e88c445f8d1
Berkayszk <testmail>
2024-02-04 17:22:24 +0300

    .

--- Fi Wo/Model/Title.swift
+++ Fi Wo/Model/Title.swift
@@ -1,17 +1,17 @@
 //
-//  Tv.swift
+//  Movie.swift
 //  Fi Wo
 //
-//  Created by Berkay Sazak on 27.01.2024.
+//  Created by Berkay Sazak on 23.01.2024.
 //
 
 import Foundation
 
-struct TrendingTvResponse: Codable {
-    let results: [Tv]
+struct TrendingTitleResponse : Codable {
+    let results : [Title]
 }
 
-struct Tv: Codable {
+struct Title : Codable{
     let id: Int
     let media_type: String?
     let original_name: String?
@@ -20,5 +20,4 @@ struct Tv: Codable {
     let vote_count: Int
     let relase_date: String?
     let vote_average: Double
-  
 }
