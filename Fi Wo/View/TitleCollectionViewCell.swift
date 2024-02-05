0764f9e47eaccbf5583a52574d17921d2df017b9
Berkayszk <testmail>
2024-02-05 21:17:54 +0300

    .

---
+++ Fi Wo/View/TitleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TitleCollectionViewCell.swift
+//  Fi Wo
+//
+//  Created by Berkay Sazak on 5.02.2024.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+}
