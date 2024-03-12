1717b8b1a04f708edd741f4fd43108901ed07a9b
Berkayszk <testmail>
2024-03-13 02:59:54 +0300

    Title TableviewCell

---
+++ Fi Wo/ViewModel/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Fi Wo
+//
+//  Created by Berkay Sazak on 13.03.2024.
+//
+
+import Foundation
---
+++ Fi Wo/ViewModel/TitleViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TitleViewModel.swift
+//  Fi Wo
+//
+//  Created by Berkay Sazak on 13.03.2024.
+//
+
+import Foundation
